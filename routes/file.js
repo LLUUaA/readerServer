2b8246243ffbc0d1c509e841e0fd796eef03e013
wqs <wuqs@bubaocloud.com>
2018-08-28 18:29:30 +0800

    file 读取md文件

--- routes/file.js
+++ routes/file.js
@@ -2,35 +2,37 @@ var Router = require('koa-router');
 var router = new Router();
 const { generateToken } = require('../utils/common');
 const fileUpload = require('../lib/upload/fileUpload');
-
+const iconv = require('iconv-lite');
 
 router.get('/', async (ctx, next) => {
     const fs = require("fs");
     // const path = process.cwd() + '/upload/README.md';
     const path = 'upload/README.md';
-    let data = '',
-        size = 0;
+    var result;
+    var datas = [];
+    var size = 0;
     const getContent = () => {
         return new Promise((resolve,reject)=>{
             fs.open(path, 'r', (err, fd) => {
                 if (err) throw err
                 const rstrem = fs.createReadStream('', { fd });
                 rstrem.on('data', (oData) => {
-                    // console.log(data);
-                    data += oData;
-                    size += oData.length;
+                    datas.push(oData);  
+                    size += oData.length;  
                 })
                 rstrem.on('end', (data) => {
                     console.log('end');
-                    data = (data||'').toString();
-                    resolve();
+                    var buff = Buffer.concat(datas, size);  
+                    var result = iconv.decode(buff, "utf8");
+                    resolve(result);
                 })
             })
         })
     }
 
-    await getContent();
-    ctx.body = data || 'file route';
+    await getContent().then(res => result = res);
+
+    ctx.body = result || 'file route';
 })
 
 /**
