3a69787fd152817c8d24e36f6336f00ecf5a4c66
PENGLUaA <962073192@qq.com>
2018-08-30 22:22:15 +0800

    clearFun

--- runSpider.js
+++ runSpider.js
@@ -1,7 +1,49 @@
 
 (() => {
-    const runSpider = require('./lib/spiderBook');
+    const runSpider = require('./lib/newSpider');
     const logger = require('./lib/logger');
     logger('runSpider','log')
-    runSpider();
+
+
+    const fs = require('fs');
+    const MAX_BOOK_ID = 10415;
+    let bookId = 1;
+
+    const clearFun = (bookId) => {
+        const basePath = `xiashula/${bookId}`;
+        const endPath = `${basePath}/chapter_end.txt`;
+        const firstCharpter = `${basePath}/chapter_1.txt`;
+
+        const next = (bookId) => {
+            clearFun(bookId + 1);
+        };
+
+        const tryAgain = (bookId) => {
+            clearFun(bookId);
+        };
+
+        //如果不存在book id
+        if (!fs.existsSync(basePath)) {
+            next(bookId);
+            return;
+        }
+
+        if (!fs.existsSync(firstCharpter) && fs.existsSync(endPath)) {
+            fs.unlink(endPath, err => {
+                if (err) {
+                    tryAgain(bookId);
+                    console.log(' delete try again ==>', bookId);
+                    return;
+                }
+                next(bookId);
+                console.log(' delete book ==>', bookId);
+            });
+
+        } else {
+            next(bookId);
+        }
+    }
+
+    // clearFun(bookId);
+
 })();
