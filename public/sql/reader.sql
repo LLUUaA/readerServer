f2e424a0f391ae54ad0854a053a666cfeaac6513
lluuaa <1012758795@qq.com>
2020-03-11 11:36:48 +0800

    - sql

--- public/sql/reader.sql
+++ public/sql/reader.sql
@@ -1,41 +1,86 @@
+/*
+Navicat MySQL Data Transfer
 
--- 创建数据库
-CREATE SCHEMA `reader`;
+Source Server         : local-qing
+Source Server Version : 80011
+Source Host           : localhost:3306
+Source Database       : reader
 
--- CREATE user table
-CREATE TABLE `reader`.`user` (
-  `id` INT(10) NOT NULL AUTO_INCREMENT,
-  `openid` VARCHAR(30) NOT NULL,
-  `nick_name` VARCHAR(50) NULL,
-  `register_time` INT(11) NULL,
-  `login_time` INT(11) NULL,
-  `login_ip` VARCHAR(45) NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`, `openid`));
+Target Server Type    : MYSQL
+Target Server Version : 80011
+File Encoding         : 65001
 
--- CREATE user history
-CREATE TABLE `reader`.`history` (
-  `id` INT NOT NULL DEFAULT 1,
-  `user_id` INT(10) NOT NULL,
-  `chapter_id` VARCHAR(10) NOT NULL COMMENT '书籍id',
-  `chapter_num` VARCHAR(10) NOT NULL COMMENT '章节',
-  `time` INT(11) NOT NULL COMMENT '时间',
-  PRIMARY KEY (`id`));
+Date: 2020-03-11 11:16:04
+*/
 
--- CREATE user bookshelf
-CREATE TABLE `reader`.`bookshelf` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` VARCHAR(10) NOT NULL,
-  `chapter_id` VARCHAR(10) NOT NULL,
-  `time` INT(11) NOT NULL,
-  `book_info` TEXT(255) NULL  COMMENT '序列化对象',
-  PRIMARY KEY (`id`));
+SET FOREIGN_KEY_CHECKS=0;
 
--- CREATE user search_record
-CREATE TABLE `reader`.`search_record` (
-  `id` INT NOT NULL DEFAULT 1,
-  `user_id` INT(10) NOT NULL,
-  `keyword` VARCHAR(45) NOT NULL,
-  `time` INT(11) NULL,
-  PRIMARY KEY (`id`))
-COMMENT = 'search keyword record';
+-- ----------------------------
+-- Table structure for bookshelf
+-- ----------------------------
+DROP TABLE IF EXISTS `bookshelf`;
+CREATE TABLE `bookshelf` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `book_info` tinytext COMMENT '序列化对象',
+  `status` tinyint(2) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for history
+-- ----------------------------
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `book_id` int(11) NOT NULL COMMENT '书籍id',
+  `chapter_num` int(5) NOT NULL COMMENT '章节',
+  `time` datetime NOT NULL COMMENT '时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- ----------------------------
+-- Table structure for search_record
+-- ----------------------------
+DROP TABLE IF EXISTS `search_record`;
+CREATE TABLE `search_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `keyword` varchar(45) NOT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='search keyword record';
+
+-- ----------------------------
+-- Table structure for session
+-- ----------------------------
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `access_token` varchar(32) NOT NULL COMMENT 'token',
+  `session_type` varchar(20) NOT NULL DEFAULT 'api' COMMENT 'session类型',
+  `expire_time` int(11) NOT NULL COMMENT '过期时间',
+  `status` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`,`user_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='session';
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(30) NOT NULL,
+  `nick_name` varchar(50) DEFAULT NULL,
+  `password` varchar(64) NOT NULL,
+  `register_time` datetime DEFAULT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `login_ip` varchar(45) DEFAULT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`,`openid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
